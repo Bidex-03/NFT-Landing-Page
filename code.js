44e97d81158cbcbed2e95100da6cd491653b4a57
Bidex-03 <abdullahqaasim14@gmail.com>
2022-08-20 09:52:11 +0100

    Changes

---
+++ code.js
@@ -0,0 +1,9 @@
+var sideLink = document.getElementById('sideLink');
+
+        function showMenu() {
+            sideLink.style.right = "0";
+        }
+
+        function hideMenu() {
+            sideLink.style.right = "-200px";
+        }
