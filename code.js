9f8fb77cb2b593882589a45b96badd6efadd3220
Bidex-03 <abdullahqaasim14@gmail.com>
2022-09-07 07:09:32 +0100

    changes

--- code.js
+++ code.js
@@ -1,25 +1,9 @@
-// var sideLink = document.getElementById('sideLink');
-
-//         function showMenu() {
-//             sideLink.style.right = "0";
-//             // sideLink.style.display = "inline-block";
-//         }
-        
-//         function hideMenu() {
-//             // sideLink.style.display = "none";
-//             // sideLink.style.transition = "1s";
-//             sideLink.style.display = "-200px";
-//         }
-
-
 var sideLink = document.getElementById('sideLink');
 
 function showMenu() {
     sideLink.style.right = "0";
-    // sideLink.style.display = "flex";
 }
 
 function hideMenu() {
     sideLink.style.right = "-200px";
-    // sideLink.style.display = "none";
 }
